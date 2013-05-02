e5c43042c095e10ba709943635c76252e43bc004
Jeremy Kelley <jeremy@33ad.org>
2013-05-02 14:08:36 -0500

    refactored the bot commands to make it easier to add more

--- turn.js
+++ turn.js
@@ -4,26 +4,51 @@ var cfg = require('./config');
 var bot = new Bot(cfg.Auth, cfg.UserId, cfg.RoomId);
 var amDj = false;
 
+
+var cmd_bop = function(data) {
+  bot.vote('up')
+};
+cmd_bop.help = 'vote for a song and dance';
+
+var cmd_dj = function(data) {
+  if (! amDj) {
+    bot.addDj();
+    bot.speak('dj ON');
+  } else {
+    bot.remDj();
+    bot.speak('dj OFF');
+  }
+  amDj = !amDj;
+}
+cmd_dj.help = 'toggle bot dj mode';
+
+var cmd_skip = function(data) {
+  bot.skip();
+  bot.speak('skipping song. As if I care...');
+};
+cmd_skip.help = 'skip song if bot is djing'
+
+var cmd_help = function(data) {
+  for (var c in cmds) {
+    bot.speak(c + ': ' + cmds[c].help);
+  }
+};
+cmd_help.help = 'this cruft';
+
+var cmds = {
+  bop: cmd_bop,
+  dj: cmd_dj,
+  skip: cmd_skip,
+  help: cmd_help
+}
+
 bot.on('speak', function (data) {
-
-  if (data.text.match(/^\/bop$/)) {
-    bot.vote('up');
-  }
-
-  if (data.text.match(/^\/dj$/)) {
-    if (! amDj) { console.log('adding as dj'); bot.addDj();}
-    else { console.log('removing dj'); bot.remDj(); }
-    amDj = !amDj;
-    bot.speak('ok.  dj=' + amDj);
-  }
-
-  if (data.text.match(/^\/skip$/)) {
-    bot.skip();
-    bot.speak('skipping song. As if I care...');
-  }
-
-  if (data.text.match(/^\/help$/)) {
-    bot.speak('help (this), dj (turn dj for bot on or off), bop (vote up), skip (skip own song if djing)');
+  for (var c in cmds) {
+    var re = new RegExp('^\/' + c + '$');
+    if (data.text.match(re)) {
+      cmds[c](data);
+      break;
+    }
   }
 });
 
